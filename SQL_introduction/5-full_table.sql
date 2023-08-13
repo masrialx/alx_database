e00497d0444e1e2eb82606dd4d3fc0c001445702
Masri Alemu <masrialemu@gmail.com>
2023-08-13 23:47:06 +0300

    v5.7

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,17 +1,13 @@
--- This script prints the full description of the table first_table from the specified database.
+-- Create database and table
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+USE hbtn_test_db_5;
 
--- Set the database name.
-SET @db_name = 'hbtn_0c_0';
-
--- Get the table name.
-SET @table_name = 'first_table';
-
--- Build the query to retrieve the full description.
-SET @query = CONCAT('SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA',
-                    ' FROM information_schema.columns',
-                    ' WHERE TABLE_SCHEMA = \'', @db_name, '\' AND TABLE_NAME = \'', @table_name, '\';');
-
--- Execute the query.
-PREPARE stmt FROM @query;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+-- Create table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
