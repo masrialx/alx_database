14cc291b6b94255182fbddb5365923d34e5d6580
Masri Alemu <masrialemu@gmail.com>
2023-08-13 22:21:15 +0300

    v1.0

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- 1-create_database_if_missing.sql
+-- This script creates the database hbtn_0c_0 if it doesn't already exist.
+
+-- Attempt to create the database if it doesn't exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
