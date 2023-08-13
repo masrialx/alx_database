b4df6038fdbbb491fd66f7a2218ec5f5bb64c64c
Masri Alemu <masrialemu@gmail.com>
2023-08-13 23:13:45 +0300

    v3.15

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,11 +1,13 @@
 -- 3-list_tables.sql
--- This script deletes and creates a new database named hbtn_test_db_0 and creates 3 tables within it.
+-- This script lists all the tables in a specified database, deletes and creates a new database named hbtn_test_db_0,
+-- and creates 3 tables within it.
+
+-- List all the tables in the specified database.
+SHOW TABLES;
 
 -- Delete and create database hbtn_test_db_0.
 DROP DATABASE IF EXISTS hbtn_test_db_0;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
-
--- Use the newly created database.
 USE hbtn_test_db_0;
 
 -- Create the first table.
@@ -23,6 +25,3 @@ CREATE TABLE IF NOT EXISTS holbteron_2 (
     id INT,
     name VARCHAR(256)
 );
-
--- Output message for "new database with 3 tables".
-SELECT 'Correct output: new database with 3 tables' AS Message;
