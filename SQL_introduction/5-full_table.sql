f973e94687cee4a979e7196eb00b58df22dbb5de
Masri Alemu <masrialemu@gmail.com>
2023-08-13 23:50:24 +0300

    v5.9

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,22 +1,11 @@
--- Create database and table
-DROP DATABASE IF EXISTS hbtn_test_db_5;
+-- Create database if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
 USE hbtn_test_db_5;
 
--- Create table
+-- Create or update table structure
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(128),
     c CHAR(1),
-    created_at DATE,
-    PRIMARY KEY (id)
+    created_at DATE
 );
-
--- Correct output: first_table
--- CREATE TABLE `first_table` (
--- `id` int(11) NOT NULL AUTO_INCREMENT,
--- `name` varchar(128) DEFAULT NULL,
--- `c` char(1) DEFAULT NULL,
--- `created_at` date DEFAULT NULL,
--- PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
