898a7cc57bb0590edf14c07d2e88f0a1328fc963
Masri Alemu <masrialemu@gmail.com>
2023-08-13 22:26:22 +0300

    v3.0

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,6 @@
+-- 3-list_tables.sql
+-- This script lists all the tables of a specified database in your MySQL server.
+
+-- Get the database name passed as an argument and list its tables.
+USE `mysql`;
+SHOW TABLES;
