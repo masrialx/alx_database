81bab43136b543b66a758afe63f6a35ae2a6a2f0
Masri Alemu <masrialemu@gmail.com>
2023-08-13 22:38:57 +0300

    v3.5

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,27 +1,8 @@
--- 4-create_tables.sql
--- This script deletes and creates the hbtn_test_db_0 database and creates 3 tables.
+-- 3-list_tables.sql
+-- This script lists all the tables of a specified database in your MySQL server.
 
--- Delete the database if it exists.
-DROP DATABASE IF EXISTS hbtn_test_db_0;
+-- Use the specified database.
+USE mysql;
 
--- Create the database.
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
-
--- Use the newly created database.
-USE hbtn_test_db_0;
-
--- Create the first table.
-CREATE TABLE IF NOT EXISTS holbteron_0 (
-    id INT
-);
-
--- Create the second table.
-CREATE TABLE IF NOT EXISTS holbteron_1 (
-    name VARCHAR(256)
-);
-
--- Create the third table.
-CREATE TABLE IF NOT EXISTS holbteron_2 (
-    id INT,
-    name VARCHAR(256)
-);
+-- Show the list of tables.
+SHOW TABLES;
