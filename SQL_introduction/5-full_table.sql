11d4bfd3a2584485768ac1b0ab5b8e58bda20af9
Masri Alemu <masrialemu@gmail.com>
2023-08-13 23:55:28 +0300

    v5.11

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,16 +1,4 @@
--- Create database and table
-DROP DATABASE IF EXISTS hbtn_test_db_5;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
-USE hbtn_test_db_5;
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128),
-    c CHAR(1),
-    created_at DATE,
-    PRIMARY KEY (id)
-);
-
 -- Print the full description of the first_table.
-SELECT GROUP_CONCAT(column_type ORDER BY ordinal_position SEPARATOR ',\n') AS Create_Table
+SELECT GROUP_CONCAT(column_type, ' ', column_name ORDER BY ordinal_position SEPARATOR ',\n') AS Create_Table
 FROM information_schema.columns
-WHERE TABLE_SCHEMA = 'hbtn_test_db_5' AND TABLE_NAME = 'first_table';
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
