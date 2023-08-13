f8aeb55de7b6cc5b4f4eeaed6d2a56a277fed2b0
Masri Alemu <masrialemu@gmail.com>
2023-08-13 22:54:26 +0300

    v3.8

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,12 +1,15 @@
 -- 3-list_tables.sql
 -- This script deletes and creates a new database named hbtn_test_db_0 without tables,
--- creates 3 tables within it, and then lists all the tables in the specified database.
+-- indicates the creation of the new database, and then lists all the tables in the specified database.
 
 -- Drop the database if it exists.
 DROP DATABASE IF EXISTS hbtn_test_db_0;
 
 -- Create the database.
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+
+-- Print a message indicating the creation of the new database without tables.
+SELECT 'New database without tables created: hbtn_test_db_0' AS Message;
 
 -- Use the newly created database.
 USE hbtn_test_db_0;
