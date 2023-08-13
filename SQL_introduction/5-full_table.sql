ef3414b14c04f7a3e7404e415dc63e3fae561fee
Masri Alemu <masrialemu@gmail.com>
2023-08-13 23:23:56 +0300

    v5.4

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
 -- 5-full_table_and_create_db_table.sql
 -- This script creates a new database named hbtn_test_db_5, a table named first_table,
--- and then prints the full description of the table first_table from the database hbtn_0c_0.
+-- and then prints the full description of the table first_table from the database hbtn_test_db_5.
 
 -- Create database and use it.
 DROP DATABASE IF EXISTS hbtn_test_db_5;
@@ -16,10 +16,7 @@ CREATE TABLE IF NOT EXISTS first_table (
     PRIMARY KEY (id)
 );
 
--- Connect to the specified database.
-USE hbtn_0c_0;
-
 -- Print the full description of the first_table.
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
 FROM information_schema.columns
-WHERE TABLE_NAME = 'first_table';
+WHERE TABLE_SCHEMA = 'hbtn_test_db_5' AND TABLE_NAME = 'first_table';
