1227b483051af801c09f6e00c913de0528c231fe
Masri Alemu <masrialemu@gmail.com>
2023-08-13 23:57:29 +0300

    v5.12

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,8 @@
 -- Print the full description of the first_table.
-SELECT GROUP_CONCAT(column_type, ' ', column_name ORDER BY ordinal_position SEPARATOR ',\n') AS Create_Table
-FROM information_schema.columns
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
+SELECT CONCAT('CREATE TABLE `first_table` (\n',
+              '  `id` int(11) NOT NULL AUTO_INCREMENT,\n',
+              '  `name` varchar(128) DEFAULT NULL,\n',
+              '  `c` char(1) DEFAULT NULL,\n',
+              '  `created_at` date DEFAULT NULL,\n',
+              '  PRIMARY KEY (`id`)\n',
+              ') ENGINE=InnoDB DEFAULT CHARSET=latin1;') AS Create_Table;
