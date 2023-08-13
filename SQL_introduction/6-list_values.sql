4a99fe0b8549f0ad744d3ec35aeb24738b342e54
Masri Alemu <masrialemu@gmail.com>
2023-08-13 23:39:34 +0300

    v6.5

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,18 +1,2 @@
--- Create database 
-DROP DATABASE IF EXISTS hbtn_test_db_6;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_6;
-USE hbtn_test_db_6;
-
--- Create table
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
-
--- Insert 3 records
-INSERT INTO first_table (id, name) VALUES (1, "First name");
-INSERT INTO first_table (id, name) VALUES (2, "Last name");
-INSERT INTO first_table (id, name) VALUES (3, "name");
-
 -- List all rows of the table first_table from the specified database.
-SELECT * FROM first_table;
+SELECT * FROM hbtn_test_db_6.first_table;
