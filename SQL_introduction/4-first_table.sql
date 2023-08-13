0e46c179e29bf4d30278385c541837b0eac428c6
Masri Alemu <masrialemu@gmail.com>
2023-08-13 23:15:26 +0300

    v4.2

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,32 +1,8 @@
--- main_0_0.sql
--- This script creates a table called first_table, inserts values, and prints the count.
+-- 4-first_table.sql
+-- This script creates a table called first_table in the current database.
 
--- Use the specified database.
-USE hbtn_test_db_4;
-
--- Create the first_table if it doesn't exist.
+-- Create the table if it doesn't exist.
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
-
--- Insert values into first_table.
-INSERT INTO first_table (id, name) VALUES (1, "First name");
-INSERT INTO first_table (id, name) VALUES (2, "Last name");
-
--- Print the count of records in first_table using a stored procedure.
-DELIMITER //
-CREATE PROCEDURE CountRecords()
-BEGIN
-    DECLARE recordCount INT;
-    SET recordCount = 0;
-
-    SELECT COUNT(*) INTO recordCount FROM information_schema.tables WHERE table_name = 'first_table';
-
-    SELECT recordCount;
-END;
-//
-DELIMITER ;
-
--- Call the stored procedure to print the count of records.
-CALL CountRecords();
