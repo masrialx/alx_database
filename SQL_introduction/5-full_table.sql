3b060fc9e9f95e8f3fad58d360c2b5701668b95e
Masri Alemu <masrialemu@gmail.com>
2023-08-16 14:26:10 +0300

    v6.0.5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,21 +1,7 @@
--- Create database and table
-DROP DATABASE IF EXISTS hbtn_test_db_5;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
-USE hbtn_test_db_5;
+-- Use the information_schema database to retrieve table structure
+USE information_schema;
 
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) DEFAULT NULL, 
-    c CHAR(1) DEFAULT NULL,
-    created_at DATE DEFAULT NULL,
-    PRIMARY KEY(id)
-);
-
-SELECT 
-    CONCAT('first_table',
-           REPLACE(
-               REPLACE(
-                   (SELECT CREATE_TABLE FROM information_schema.TABLES WHERE TABLE_SCHEMA='hbtn_test_db_5' AND TABLE_NAME='first_table'), 
-                   '\n', ' '), 
-               '`', '')
-           ) AS 'Table Create Table';
+-- Select relevant information about columns from the COLUMNS table
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
+FROM COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
