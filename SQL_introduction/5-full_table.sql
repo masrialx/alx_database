b64baf19bb89ed24cd7f262adddd69df838c5c23
Masri Alemu <masrialemu@gmail.com>
2023-08-16 14:10:38 +0300

    v5.5.5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,4 @@
+-- Print the full description of the table first_table from the specified database.
 SELECT table_name AS 'Table', create_table AS 'Create Table'
 FROM information_schema.tables 
 WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
