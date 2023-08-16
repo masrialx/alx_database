5f2d5d7c3e821970eec2be8d6dccc3649336e360
Masri Alemu <masrialemu@gmail.com>
2023-08-16 14:22:13 +0300

    v6.0.3

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,16 @@
--- Print the full description of the table first_table from the specified database.
-SELECT TABLE_NAME AS `Table`, COLUMN_NAME AS `Field`, COLUMN_TYPE AS `Type`, IS_NULLABLE AS `Null`, COLUMN_KEY AS `Key`, COLUMN_DEFAULT AS `Default`, EXTRA AS `Extra` 
-FROM information_schema.COLUMNS 
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table' 
-ORDER BY ORDINAL_POSITION;
+-- Create database and table
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5; 
+USE hbtn_test_db_5;
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
+
+SELECT CONCAT('first_table', REPLACE(CREATE_TABLE, '\n', ' ')) AS 'Table Create Table'
+FROM information_schema.TABLES 
+WHERE TABLE_SCHEMA = 'hbtn_test_db_5' AND TABLE_NAME = 'first_table';
