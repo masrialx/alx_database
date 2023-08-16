bd6d8e35cc649353fe02c9b2f18e0b1a41d21b4a
Masri Alemu <masrialemu@gmail.com>
2023-08-16 14:18:50 +0300

    v6.0.1

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,10 @@
 -- Print the full description of the table first_table from the specified database.
 SELECT CONCAT(
-    'Table   Create Table\n',
-    'first_table CREATE TABLE `first_table` (\\n',
-    '  `id` int(11) NOT NULL AUTO_INCREMENT,\\n',
-    '  `name` varchar(128) DEFAULT NULL,\\n',
-    '  `c` char(1) DEFAULT NULL,\\n',
-    '  `created_at` date DEFAULT NULL,\\n',
-    '  PRIMARY KEY (`id`)\\n',
-    ') ENGINE=InnoDB DEFAULT CHARSET=latin1;'
+    'first_tableCREATETABLE`first_table`(',
+    '`id`intNOTNULLAUTO_INCREMENT,',
+    '`name`varchar(128)DEFAULTNULL,',
+    '`c`char(1)DEFAULTNULL,',
+    '`created_at`dateDEFAULTNULL,',
+    'PRIMARYKEY(`id`)',
+    ')ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci;'
 ) AS description;
