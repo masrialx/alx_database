f01411db69a75a62cc90288a3dc3c4a674fff29f
Masri Alemu <masrialemu@gmail.com>
2023-08-16 14:39:06 +0300

    v5.57

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,8 @@
 -- Retrieve the full description of the first_table from the hbtn_0c_0 database
-USE information_schema;
-
--- Select column details for the first_table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
-FROM COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SELECT
+  GROUP_CONCAT(
+    CONCAT('`', COLUMN_NAME, '`', COLUMN_TYPE, IF(IS_NULLABLE = 'NO', 'NOT NULL', ''), IF(COLUMN_DEFAULT IS NOT NULL, CONCAT('DEFAULT ', COLUMN_DEFAULT), ''), IF(COLUMN_KEY = 'PRI', 'PRIMARY KEY', ''), IF(EXTRA = 'auto_increment', 'AUTO_INCREMENT', '')) SEPARATOR ',\n') AS CreateTable
+FROM
+  information_schema.COLUMNS
+WHERE
+  TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
