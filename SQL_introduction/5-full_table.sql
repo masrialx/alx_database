019c1c2d1d9599b5113571a7ae07305e0d5e71e0
Masri Alemu <masrialemu@gmail.com>
2023-08-16 14:37:43 +0300

    v5.53

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,7 @@
--- Show the CREATE TABLE statement for first_table from hbtn_0c_0 database
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+-- Retrieve the full description of the first_table from the hbtn_0c_0 database
+USE information_schema;
+
+-- Select column details for the first_table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+FROM COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
