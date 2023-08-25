6084ce03ae9cf2aef9304a7a0be2d396ab8b721f
Masri Alemu <masrialemu@gmail.com>
2023-08-25 22:18:00 +0300

    v0.0

---
+++ SQL_introduction/SQL_more_queries/0-privileges.sql
@@ -0,0 +1,17 @@
+# Create user user_0d_1
+echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+
+# Grant privileges to user_0d_1
+echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+
+# Create user user_0d_2
+echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+
+# Grant privileges to user_0d_2
+echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+
+# List privileges for user_0d_1
+echo "SHOW GRANTS FOR 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+
+# List privileges for user_0d_2
+echo "SHOW GRANTS FOR 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
