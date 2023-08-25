c8fe3dd1d81ccb8fc60ea2eaa3b3bd9c2cd644ba
Masri Alemu <masrialemu@gmail.com>
2023-08-25 22:30:34 +0300

    v0.6

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,23 @@
 -- 0-privileges.sql
 -- This script creates MySQL users and lists their privileges.
 
--- Create user user_0d_1 and grant privileges
-CREATE USER 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Check if user_0d_1 exists before creating and granting privileges
+SELECT user FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost' INTO @user_0d_1_exists;
 
--- Create user user_0d_2 and grant privileges
-CREATE USER 'user_0d_2'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+-- If user_0d_1 doesn't exist, create and grant privileges
+IF @user_0d_1_exists IS NULL THEN
+    CREATE USER 'user_0d_1'@'localhost';
+    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+END IF;
+
+-- Check if user_0d_2 exists before creating and granting privileges
+SELECT user FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost' INTO @user_0d_2_exists;
+
+-- If user_0d_2 doesn't exist, create and grant privileges
+IF @user_0d_2_exists IS NULL THEN
+    CREATE USER 'user_0d_2'@'localhost';
+    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+END IF;
 
 -- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
