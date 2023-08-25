8b970a9449324fb6c91e58b1312af06887b6d12c
Masri Alemu <masrialemu@gmail.com>
2023-08-25 22:50:05 +0300

    v3.3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,15 +1,11 @@
--- New database hbtn_test_db_3
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_3;
+-- 3-force_name.sql
+-- This script creates the table force_name on your MySQL server.
 
--- Use the new database
-USE hbtn_test_db_3;
+-- Use the specified database (hbtn_0d_2 in this case)
+USE hbtn_0d_2;
 
--- Regular inserts + select
--- (Assuming the table force_name does not exist yet)
-INSERT INTO force_name (id, name) VALUES (1, "Holberton School");
-INSERT INTO force_name (id, name) VALUES (2, "Holberton");
-INSERT INTO force_name (id, name) VALUES (3, "School");
-INSERT INTO force_name (id, name) VALUES (4, "C is fun");
-INSERT INTO force_name (id, name) VALUES (1, "Python is cool");
-
-SELECT * FROM force_name ORDER BY id, name ASC;
+-- Create table force_name if it doesn't exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
