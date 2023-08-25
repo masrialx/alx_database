8ca3d3bbf53212da3230c50a44a2a85be740a2cd
Masri Alemu <masrialemu@gmail.com>
2023-08-25 23:34:16 +0300

    v8.3

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,12 @@
+-- Use the hbtn_0d_usa database
 USE hbtn_0d_usa;
 
+-- Select the id of the state with name 'California'
+SELECT id
+FROM states
+WHERE name = 'California';
+
+-- Select the cities in California and order by id
 SELECT id, name
 FROM cities
 WHERE state_id = (
