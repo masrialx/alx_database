55c768bf3470d2bf42155bfa035c4919ef6310b0
Masri Alemu <masrialemu@gmail.com>
2023-08-25 22:54:23 +0300

    v3.6

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,24 +1,18 @@
--- 3-force_name.sql
--- This script creates the table force_name if it doesn't exist and performs inserts.
-
--- Use the specified database (hbtn_0d_2 in this case)
-USE hbtn_0d_2;
-
--- Drop table force_name if it exists
-DROP TABLE IF EXISTS force_name;
-
--- Create table force_name
-CREATE TABLE force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
-);
-
--- Insert data into the table
+-- Regular inserts + select
+-- Insert data into the force_name table
 INSERT INTO force_name (id, name) VALUES (1, "Holberton School");
 INSERT INTO force_name (id, name) VALUES (2, "Holberton");
 INSERT INTO force_name (id, name) VALUES (3, "School");
 INSERT INTO force_name (id, name) VALUES (4, "C is fun");
 INSERT INTO force_name (id, name) VALUES (1, "Python is cool");
 
--- Describe the table to show its structure
-DESCRIBE force_name;
+-- Select data from the force_name table
+-- This will show the content of the force_name table in ascending order of id and name
+SELECT * FROM force_name ORDER BY id, name ASC;
+
+-- New database hbtn_test_db_3
+-- Drop the hbtn_test_db_3 database if it exists
+DROP DATABASE IF EXISTS hbtn_test_db_3;
+
+-- Create the hbtn_test_db_3 database
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_3;
