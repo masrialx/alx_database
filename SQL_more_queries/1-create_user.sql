ad49fb3dd8eaf4e5adfaefb2bf5135b3f28d9bf6
Masri Alemu <masrialemu@gmail.com>
2023-08-25 22:37:26 +0300

    v1.0

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- 1-create_user.sql
+-- This script creates the MySQL user user_0d_1 with all privileges.
+
+-- Create user user_0d_1 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
