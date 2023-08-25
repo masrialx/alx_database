68ae7e2eb52de2661650236b2b90233290342645
Masri Alemu <masrialemu@gmail.com>
2023-08-25 22:40:41 +0300

    v2.0

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- 2-create_read_user.sql
+-- This script creates the database hbtn_0d_2 and the user user_0d_2.
+
+-- Create database hbtn_0d_2 if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user user_0d_2 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege to user_0d_2 on hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
