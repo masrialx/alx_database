b2ec5d170707addc79dc34f29dedc081433b7bcc
Masri Alemu <masrialemu@gmail.com>
2023-08-25 23:06:38 +0300

    v6.1

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -9,6 +9,3 @@ CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
-
--- Insert sample data into the states table
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");
