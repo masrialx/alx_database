e399ddf36cffba3479681e34df0704137b759c8c
Masri Alemu <masrialemu@gmail.com>
2023-08-25 23:52:44 +0300

    v8.7

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,17 +1,10 @@
-USE hbtn_0d_usa;
-
--- Retrieve the state_id for California
-SET @california_id = (SELECT id FROM states WHERE name = 'California');
-
--- Check if there are cities in California
-SET @city_count = (SELECT COUNT(*) FROM cities WHERE state_id = @california_id);
-
--- Display results based on city count
-IF @city_count > 0 THEN
-    SELECT id, name
-    FROM cities
-    WHERE state_id = @california_id
-    ORDER BY id ASC;
-ELSE
-    SELECT 'No cities in California';
-END IF;
+-- List all cities of California using a subquery
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+-- Order the results in ascending order by city IDs
+ORDER BY id ASC;
