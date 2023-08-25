42afba92905092d3e9a96d245ec8d83507e465d8
Masri Alemu <masrialemu@gmail.com>
2023-08-25 23:33:21 +0300

    v8.2

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,21 +1,10 @@
 USE hbtn_0d_usa;
 
--- List cities in California
-SELECT cities.id, cities.name
+SELECT id, name
 FROM cities
-WHERE cities.state_id = (
-    SELECT states.id
+WHERE state_id = (
+    SELECT id
     FROM states
-    WHERE states.name = 'California'
+    WHERE name = 'California'
 )
-ORDER BY cities.id;
-
--- List cities in a non-existent state (empty listing)
-SELECT cities.id, cities.name
-FROM cities
-WHERE cities.state_id = (
-    SELECT states.id
-    FROM states
-    WHERE states.name = 'NonExistentState'
-)
-ORDER BY cities.id;
+ORDER BY id;
