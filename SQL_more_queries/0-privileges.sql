c2b6ae758280c8ec7337d2074b4f36d7ea6194f6
Masri Alemu <masrialemu@gmail.com>
2023-08-25 22:34:15 +0300

    v0.7

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,20 @@
 -- 0-privileges.sql
--- This script deletes MySQL users if they exist.
+-- This script creates MySQL users and lists their privileges.
 
--- Drop user user_0d_1 if it exists
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
+-- Create user user_0d_1 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 
--- Drop user user_0d_2 if it exists
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Create user user_0d_2 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Grant all privileges to user_0d_2
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
